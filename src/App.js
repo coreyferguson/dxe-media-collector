495fa01e9f0341c3ef45873ee638676fa0daa5e6
Corey Ferguson <corey.t.ferguson@gmail.com>
2020-02-08 12:51:49 -0800

    loading state

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import getMediaService from './mediaService';
 import MediaList from './MediaList';
 
 function App() {
-  const [articles, setArticles] = React.useState([]);
+  const [ loading, setLoading ] = React.useState(true);
+  const [ articles, setArticles ] = React.useState([]);
+  const [ error, setError ] = React.useState();
   React.useEffect(() => {
-    getMediaService().fetchAll().then(res => setArticles(res.articles)).catch(err => {
-      console.log('err :', err);
-    })
+    getMediaService().fetchAll().then(res => {
+      setArticles(res.articles);
+      setLoading(false);
+    }).catch(err => {
+      console.error('error', err);
+      setLoading(false);
+      setError(err);
+    });
   }, []);
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>{error.message}</h1>
   return (
     <div className="App">
       <MediaList articles={articles} />
